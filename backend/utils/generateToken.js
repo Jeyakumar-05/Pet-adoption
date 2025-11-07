82c7b330c8a7586602de9a751780fd2ea0522ccf
Jeyakumar <jeyakumar1442@gmail.com>
2025-11-07 22:19:33 +0530

    cross domain policy added

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -5,10 +5,22 @@ const generateToken = (res, userId) => {
         expiresIn: "1d"
     });
 
+    // for local development here we are using the same site policy(http only)
+    // res.cookie("jwt", token, {
+    //     httpOnly: true,
+    //     secure: process.env.NODE_ENV || "development",
+    //     sameSite: true,
+    //     maxAge: 1 * 24 * 60 * 60 * 1000
+    // })
+
+    // for production deployment we are using the cross-domain policy(none)
+    const isProduction = process.env.NODE_ENV === "production";
+
     res.cookie("jwt", token, {
         httpOnly: true,
-        secure: process.env.NODE_ENV || "development",
-        sameSite: true,
+        secure: isProduction,
+        sameSite: "none", // required for cross-domain cookies
+        path: "/",
         maxAge: 1 * 24 * 60 * 60 * 1000
     })
 }
